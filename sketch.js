9ae27754f79a338276df2ccd0adfe5d8cc88f2bc
Sweta <71880635+SwetaPadmaja@users.noreply.github.com>
2020-11-22 11:33:47 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -62,7 +62,7 @@ rope5.display();
 function keyPressed() {
 	if (keyCode === UP_ARROW) {
 
-	  Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-50,y:-45});
+	  Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-10,y:-10});
 
 	}
 }
